f376adacf88240db8e88dc445de5aac7a8c4db37
vuffiraa72 <u.mersewsky+github@gmail.com>
2018-11-18 19:17:08 +0100

    0.4.9
    - fixed nextCleaning*
    - updated English commandref

--- 70_BOTVAC.pm
+++ 70_BOTVAC.pm
@@ -352,7 +352,7 @@ sub BOTVAC_Set($@) {
 
         return "No argument given" if ( !defined( $a[2] ) );
 
-        readingsSingleUpdate($name, $a[1], $a[2], 0);
+        readingsSingleUpdate($hash, $a[1], $a[2], 0);
     }
     
     # password
@@ -1448,179 +1448,145 @@ sub BOTVAC_GetMap() {
 
 1;
 =pod
-=item summary 		Robot Vacuums
-=item summary_DE	Staubsauger Roboter
+=item device
+=item summary     Robot Vacuums
+=item summary_DE  Staubsauger Roboter
+
 =begin html
 
 <a name="BOTVAC"></a>
 <h3>BOTVAC</h3>
+<div>
 <ul>
-  This module controls Neato Botvac Connected and Vorwerk Robot Vacuums.
+  This module controls Neato Botvac Connected and Vorwerk Robot Vacuums.<br/>
   For issuing commands or retrieving Readings it's necessary to fetch the information from the NEATO/VORWERK Server.
- In this way, it can happen, that it's not possible to send commands to the Robot until the corresponding Values are fetched. 
- This means, it can need some time until your Robot will react on your command.
-	<br><br>
-  <a name="BOTVACDefine"></a>
-  <b>Define</b>
-	<ul>
-	<code>define &lt;name&gt; BOTVAC &lt;email&gt; [NEATO|VORWERK] [&lt;polling-interval&gt;]</code>
-	<br><br>
-	Example: 
-	<code>define myNeato BOTVAC myemail@myprovider.com NEATO 300</code>
-	<br><br>
-	After defining the Device, it's necessary to enter the password with "set &lt;name&gt; password &lt;password&gt;
-	It is exactly the same Password as you use on the Website or inside the App.
-	<br><br>
-	Example: 
-	<code>set NEATO passwort mySecretPassword</code>
-	<br><br>
-	</ul>
-	 <br>
+  In this way, it can happen, that it's not possible to send commands to the Robot until the corresponding Values are fetched.
+  This means, it can need some time until your Robot will react on your command.
+  <br/><br/>
 
-  <a name="BOTVACget"></a>
-  <b>Get</b>
-  <ul>
-	<a name="batteryPercent"></a>
-	<li>
-	batteryPercent
-	<br>
-      	requests the state of the battery from Robot
-	</li>
-	<br>
-
-</ul><br>
-
-  <a name="BOTVACset"></a>
-  <b>Set</b>
+<a name="BOTVACDefine"></a>
+<b>Define</b>
 <ul>
-	<a name="findMe"></a>
-	<li>
-	<code> set <name> findMe</code>
-	<br>
-      plays a sound and let the LED light for easier finding of a stuck robot
-	</li>
-	<br>
+  <br>
+  <code>define &lt;name&gt; BOTVAC &lt;email&gt; [NEATO|VORWERK] [&lt;polling-interval&gt;]</code>
+  <br/><br/>
+  Example:&nbsp;<code>define myNeato BOTVAC myemail@myprovider.com NEATO 300</code>
+  <br/><br/>
 
-	<a name="dismissCurrentAlert"></a>
-	<li>
-	<code> set <name> dismissCurrentAlert</code>
-	<br>
-      	reset an actual Warning (e.g. dustbin full)
-	</li>
-	<br>
+  After defining the Device, it's necessary to enter the password with "set &lt;name&gt; password &lt;password&gt;"<br/>
+  It is exactly the same Password as you use on the Website or inside the App.
+  <br/><br/>
+  Example:&nbsp;<code>set NEATO passwort mySecretPassword</code>
+  <br/><br/>
+</ul>
 
-	<a name="manualCleaningMode"></a>
-	<li>
-	<code> set <name> manualCleaningMode</code>
-	<br>
-      
-	</li>
-	<br>
+<a name="BOTVACget"></a>
+<b>Get</b>
+<ul>
+<br>
+  <li><code>get &lt;name&gt; batteryPercent</code>
+  <br>
+  requests the state of the battery from Robot
+  </li><br>
+</ul>
 
-
-	<a name="nextCleaningMode"></a>
-	<li>
-	<code> set <name> nextCleaningMode</code>
-	<br>
-      
-	</li>
-	<br>
-
-
-	<a name="nextCleaningNavigationMode"></a>
-	<li>
-	<code> set <name> nextCleaningNavigationMode</code>
-	<br>
-      
-	</li>
-	<br>
-
-
-	<a name="nextCleaningSpotHeight"></a>
-	<li>
-	<code> set <name> nextCleaningSpotHeight</code>
-	<br>
-      
-	</li>
-	<br>
-
-
-	<a name="nextCleaningSpotWidth"></a>
-	<li>
-	<code> set <name> nextCleaningSpotWidth</code>
-	<br>
-      
-	</li>
-	<br>
-
-	<a name="password"></a>
-	<li>
-	<code> set <name> password <password></code>
-	<br>
-      	set the password for the NEATO/VORWERK account
-	</li>
-	<br>
-
-	<a name="pause"></a>
-	<li>
-	<code> set <name> pause</code>
-	<br>
-      	interrupts the cleaning
-	</li>
-	<br>
-
-	<a name="pauseToBase"></a>
-	<li>
-	<code> set <name> pauseToBase</code>
-	<br>
-	      stops cleaning and returns to base
-	</li>
-	<br>
-
-	<a name="reloadMaps"></a>
-	<li>
-	<code> set <name> reloadMaps</code>
-	<br>
-      	load last map from server into the cache of the module. no file is stored!
-	</li>
-	<br>
-
-	<a name="resume"></a>
-	<li>
-	<code> set <name> resume</code>
-	<br>
-       resume cleaning after pause
-	</li>
-	<br>
-
-	<a name="schedule"></a>
-	<li>
-	<code> set <name> schedule</code>
-	<br>
-      	on and off, switch time control
-	</li>
-	<br>
-
-	<a name="sendToBase"></a>
-	<li>
-	<code> set <name> sendToBase</code>
-	<br>
-      send roboter back to base
-	</li>
-	<br>
-
-	<a name="setBoundaries"></a>
-	<li>
-	<code> set <name> setBoundaries</code>
-	<br>
-      set boundaries/nogo lines
-	</li>
-	<br>
-
-	<a name="setBoundariesOnFloorplan"></a>
-	<li>
-	setBoundariesOnFloorplan_&lt;floor plan&gt; &lt;name|{JSON String}&gt;
-	<br>
+<a name="BOTVACset"></a>
+<b>Set</b>
+<ul>
+<br>
+  <li>
+  <code> set &lt;name&gt; findMe</code>
+  <br>
+  plays a sound and let the LED light for easier finding of a stuck robot
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; dismissCurrentAlert</code>
+  <br>
+        reset an actual Warning (e.g. dustbin full)
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; manualCleaningMode</code>
+  <br>
+  MISSING
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; nextCleaningMode</code>
+  <br>
+  MISSING
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; nextCleaningNavigationMode</code>
+  <br>
+  MISSING
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; nextCleaningSpotHeight</code>
+  <br>
+  MISSING
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; nextCleaningSpotWidth</code>
+  <br>
+  MISSING
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; password &lt;password&gt;</code>
+  <br>
+        set the password for the NEATO/VORWERK account
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; pause</code>
+  <br>
+        interrupts the cleaning
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; pauseToBase</code>
+  <br>
+  stops cleaning and returns to base
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; reloadMaps</code>
+  <br>
+        load last map from server into the cache of the module. no file is stored!
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; resume</code>
+  <br>
+  resume cleaning after pause
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; schedule</code>
+  <br>
+        on and off, switch time control
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; sendToBase</code>
+  <br>
+  send roboter back to base
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; setBoundaries</code>
+  <br>
+  set boundaries/nogo lines
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; setBoundariesOnFloorplan_&lt;floor plan&gt; &lt;name|{JSON String}&gt;</code>
+  <br>
     Set boundaries/nogo lines in the corresponding floor plan.<br>
     The paramter can either be a name, which is already defined by attribute "boundaries", or alternatively a JSON string.
     (A comma-separated list of names is also possible.)<br>
@@ -1631,136 +1597,104 @@ sub BOTVAC_GetMap() {
     set &lt;name&gt; setBoundariesOnFloorplan_0 Bad,Kueche<br>
     set &lt;name&gt; setBoundariesOnFloorplan_0 {"type":"polyline","vertices":[[0.710,0.6217],[0.710,0.6923]],
       "name":"Bad","color":"#E54B1C","enabled":true}
-	</li>
-	<br>
-
-	<a name="setRobot"></a>
-	<li>
-	<code> set <name> setRobot</code>
-	<br>
-      choose robot if more than one is registered at the used account
-	</li>
-	<br>
-
-	<a name="startCleaning"></a>
-	<li>
-	<code> set <name> startCleaning</code>
-	<br>
-      start the Cleaning from the scratch. Depending on Model, there are additional Arguments available: eco/turbo ; normal/extraCare
-	</li>
-	<br>
-
-
-	<a name="startSpot"></a>
-	<li>
-	<code> set <name> startSpot</code>
-	<br>
-            start spot-Cleaning from actual position. Depending on Model, there are additional Arguments available: eco/turbo ; normal/extraCare
-	</li>
-	<br>
-
-
-	<a name="statusRequest"></a>
-	<li>
-	<code> set <name> statusRequest</code>
-	<br>
-      pull update of all readings. necessary because NEATO/VORWERK does not send updates at their own.
-	</li>
-	<br>
-
-
-	<a name="stop"></a>
-	<li>
-	stop
-	<br>
-      <code> set <name> stop cleaning</code>
-	</li>
-	<br>
-
-
-	<a name="syncRobots"></a>
-	<li>
-	<code> set <name> syncRobots</code>
-	<br>
-      sync robot data with online account. Useful if one has more then one robot registered
-	</li>
-	<br>
-
-
-	<a name="stopCleaning"></a>
-	<li>
-	<code> set <name> stopCleaning</code>
-	<br>
-      stopCleaning and stay where you are
-	</li>
-	<br>
-
-
-	<a name=""></a>
-	<li>
-	
-	<br>
-      
-	</li>
-	<br>
-
-
-</ul><br>
-
-  <a name="BOTVACattr"></a>
-  <b>Attributes</b>
-  <ul>
-
-    <li><a href="#disable">disable</a><br>
-        <a href="#disabledForIntervals">disabledForIntervals</a><br>
-      disable distribution of messages. The server itself will accept and store
-      messages, but not forward them.
-      </li><br>
-	
-    <li><a href="#disable">actionInterval</a><br>
-        <a href="#disabledForIntervals">disabledForIntervals</a><br>
-      disable distribution of messages. The server itself will accept and store
-      messages, but not forward them.
-      </li><br>
-
-   <a name="actionInterval"></a>
-    <li>actionInterval<br>
-      time in seconds between status requests while Device is working
-      </li><br>
-
-	<a name="boundaries"></a>
-	<li>
-	boundaries
-	<br>
-	  Boundary entries separated by space in JSON format, e.g.<br>
-    {"type":"polyline","vertices":[[0.710,0.6217],[0.710,0.6923]],"name":"Bad","color":"#E54B1C","enabled":true}<br>
-    {"type":"polyline","vertices":[[0.7139,0.4101],[0.7135,0.4282],[0.4326,0.3322],[0.4326,0.2533],[0.3931,0.2533],
-      [0.3931,0.3426],[0.7452,0.4637],[0.7617,0.4196]],"name":"Kueche","color":"#000000","enabled":true}<br>
-    For description of syntax see: <a href>https://developers.neatorobotics.com/api/robot-remote-protocol/maps</a><br>
-    The value of paramter "name" is used as setListe for "setBoundariesOnFloorplan_&lt;floor plan&gt;".
-    It is also possible to save more than one boundary with the same name.
-    The command "setBoundariesOnFloorplan_&lt;floor plan&gt; &lt;name&gt;" sends all boundary with the same name.
-	</li>
-	<br>
-	<a name="oldreadings"></a>
-	<li>
-	oldreadings
-	<br>
-      
-	</li>
-	<br>
-  </ul>
+  </li>
+<br>
+  <code> set &lt;name&gt; setRobot</code>
+  <br>
+  choose robot if more than one is registered at the used account
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; startCleaning</code>
+  <br>
+  start the Cleaning from the scratch. Depending on Model, there are additional Arguments available: eco/turbo ; normal/extraCare
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; startSpot</code>
+  <br>
+  start spot-Cleaning from actual position. Depending on Model, there are additional Arguments available: eco/turbo ; normal/extraCare
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; statusRequest</code>
+  <br>
+  pull update of all readings. necessary because NEATO/VORWERK does not send updates at their own.
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; stop cleaning</code>
+  <br>
+  stop cleaning
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; syncRobots</code>
+  <br>
+  sync robot data with online account. Useful if one has more then one robot registered
+  </li>
+<br>
+  <li>
+  <code> set &lt;name&gt; stopCleaning</code>
+  <br>
+  stopCleaning and stay where you are
+  </li>
+<br>
 </ul>
-=end html
-=begin html_DE
-
-<a name="BOTVAC"></a>
-<h3>BOTVAC</h3>
+<a name="BOTVACattr"></a>
+<b>Attributes</b>
 <ul>
-  Dieses Module steuert Neato Botvac Connected und Vorwerk Staubsaugerroboter
-  <br><br>
-  <b>Define</b>
+<br>
+
+
+  <li>
+  <code> myFirstAttr</code>
+  <br>
+  Explanation
+  </li>
+<br>
+  <li>
+  <code>actionInterval</code>
+  <br>
+  time in seconds between status requests while Device is working
+  </li>
+<br>
+  <li>
+  <code>boundaries</code>
+  <br>
+  Boundary entries separated by space in JSON format, e.g.<br>
+  {"type":"polyline","vertices":[[0.710,0.6217],[0.710,0.6923]],"name":"Bad","color":"#E54B1C","enabled":true}<br>
+  {"type":"polyline","vertices":[[0.7139,0.4101],[0.7135,0.4282],[0.4326,0.3322],[0.4326,0.2533],[0.3931,0.2533],
+    [0.3931,0.3426],[0.7452,0.4637],[0.7617,0.4196]],"name":"Kueche","color":"#000000","enabled":true}<br>
+  For description of syntax see: <a href>https://developers.neatorobotics.com/api/robot-remote-protocol/maps</a><br>
+  The value of paramter "name" is used as setListe for "setBoundariesOnFloorplan_&lt;floor plan&gt;".
+  It is also possible to save more than one boundary with the same name.
+  The command "setBoundariesOnFloorplan_&lt;floor plan&gt; &lt;name&gt;" sends all boundary with the same name.
+  </li>
+<br>
 </ul>
+
+</ul>
+
+=end html
+
+=begin html_DE
+<a name="BOTVAC"></a>
+
+<h3>BOTVAC</h3>
+
+<ul>
+
+  Dieses Module steuert Neato Botvac Connected und Vorwerk Staubsaugerroboter
+
+  <br><br>
+
+  <b>Define</b>
+
+</ul>
+
+
 
 =end html_DE
+
 =cut
--- 70_BOTVAC_cmdref.pm
+++ 70_BOTVAC_cmdref.pm
@@ -9,9 +9,9 @@
 <h3>BOTVAC</h3>
 <div>
 <ul>
-	This module controls Neato Botvac Connected and Vorwerk Robot Vacuums.
+	This module controls Neato Botvac Connected and Vorwerk Robot Vacuums.<br/>
 	For issuing commands or retrieving Readings it's necessary to fetch the information from the NEATO/VORWERK Server.
-	In this way, it can happen, that it's not possible to send commands to the Robot until the corresponding Values are fetched. 
+	In this way, it can happen, that it's not possible to send commands to the Robot until the corresponding Values are fetched.
 	This means, it can need some time until your Robot will react on your command.
 	<br/><br/>
 
@@ -21,13 +21,13 @@
 	<br>
 	<code>define &lt;name&gt; BOTVAC &lt;email&gt; [NEATO|VORWERK] [&lt;polling-interval&gt;]</code>
 	<br/><br/>
-	Example:<code>define myNeato BOTVAC myemail@myprovider.com NEATO 300</code>
+	Example:&nbsp;<code>define myNeato BOTVAC myemail@myprovider.com NEATO 300</code>
 	<br/><br/>
 
-	After defining the Device, it's necessary to enter the password with "set &lt;name&gt; password &lt;password&gt;
+	After defining the Device, it's necessary to enter the password with "set &lt;name&gt; password &lt;password&gt;"<br/>
 	It is exactly the same Password as you use on the Website or inside the App.
 	<br/><br/>
-	Example:<code>set NEATO passwort mySecretPassword</code>
+	Example:&nbsp;<code>set NEATO passwort mySecretPassword</code>
 	<br/><br/>
 </ul>
 
@@ -37,7 +37,7 @@
 <br>
 	<li><code>get &lt;name&gt; batteryPercent</code>
 	<br>
-	requests the state of the battery hopefully directly from Robot
+	requests the state of the battery from Robot
 	</li><br>
 </ul>
 
@@ -46,133 +46,147 @@
 <ul>
 <br>
 	<li>
-	<code> set <name> findMe</code>
+	<code> set &lt;name&gt; findMe</code>
 	<br>
 	plays a sound and let the LED light for easier finding of a stuck robot
 	</li>
 <br>
 	<li>
-	<code> set <name> dismissCurrentAlert</code>
+	<code> set &lt;name&gt; dismissCurrentAlert</code>
 	<br>
       	reset an actual Warning (e.g. dustbin full)
 	</li>
 <br>
 	<li>
-	<code> set <name> manualCleaningMode</code>
+	<code> set &lt;name&gt; manualCleaningMode</code>
 	<br>
 	MISSING
 	</li>
 <br>
 	<li>
-	<code> set <name> nextCleaningMode</code>
+	<code> set &lt;name&gt; nextCleaningMode</code>
 	<br>
 	MISSING
 	</li>
 <br>
 	<li>
-	<code> set <name> nextCleaningNavigationMode</code>
+	<code> set &lt;name&gt; nextCleaningNavigationMode</code>
 	<br>
 	MISSING
 	</li>
 <br>
 	<li>
-	<code> set <name> nextCleaningSpotHeight</code>
+	<code> set &lt;name&gt; nextCleaningSpotHeight</code>
 	<br>
 	MISSING
 	</li>
 <br>
 	<li>
-	<code> set <name> nextCleaningSpotWidth</code>
+	<code> set &lt;name&gt; nextCleaningSpotWidth</code>
 	<br>
 	MISSING
 	</li>
 <br>
 	<li>
-	<code> set <name> password <password></code>
+	<code> set &lt;name&gt; password &lt;password&gt;</code>
 	<br>
       	set the password for the NEATO/VORWERK account
 	</li>
 <br>
 	<li>
-	<code> set <name> pause</code>
+	<code> set &lt;name&gt; pause</code>
 	<br>
-      	breaks the cleaning
+      	interrupts the cleaning
 	</li>
 <br>
 	<li>
-	<code> set <name> pauseToBase</code>
+	<code> set &lt;name&gt; pauseToBase</code>
 	<br>
 	stops cleaning and returns to base
 	</li>
 <br>
 	<li>
-	<code> set <name> reloadMaps</code>
+	<code> set &lt;name&gt; reloadMaps</code>
 	<br>
       	load last map from server into the cache of the module. no file is stored!
 	</li>
 <br>
 	<li>
-	<code> set <name> resume</code>
+	<code> set &lt;name&gt; resume</code>
 	<br>
 	resume cleaning after pause
 	</li>
 <br>
 	<li>
-	<code> set <name> schedule</code>
+	<code> set &lt;name&gt; schedule</code>
 	<br>
       	on and off, switch time control
 	</li>
 <br>
 	<li>
-	<code> set <name> sendToBase</code>
+	<code> set &lt;name&gt; sendToBase</code>
 	<br>
 	send roboter back to base
 	</li>
 <br>
 	<li>
-	<code> set <name> setBoundaries</code>
+	<code> set &lt;name&gt; setBoundaries</code>
 	<br>
 	set boundaries/nogo lines
 	</li>
 <br>
 	<li>
-	<code> set <name> setRobot</code>
+  <code> set &lt;name&gt; setBoundariesOnFloorplan_&lt;floor plan&gt; &lt;name|{JSON String}&gt;</code>
+	<br>
+    Set boundaries/nogo lines in the corresponding floor plan.<br>
+    The paramter can either be a name, which is already defined by attribute "boundaries", or alternatively a JSON string.
+    (A comma-separated list of names is also possible.)<br>
+    Description of syntax at <a href>https://developers.neatorobotics.com/api/robot-remote-protocol/maps</a><br>
+    <br>
+    Examples:<br>
+    set &lt;name&gt; setBoundariesOnFloorplan_0 Bad<br>
+    set &lt;name&gt; setBoundariesOnFloorplan_0 Bad,Kueche<br>
+    set &lt;name&gt; setBoundariesOnFloorplan_0 {"type":"polyline","vertices":[[0.710,0.6217],[0.710,0.6923]],
+      "name":"Bad","color":"#E54B1C","enabled":true}
+	</li>
+<br>
+	<code> set &lt;name&gt; setRobot</code>
 	<br>
 	choose robot if more than one is registered at the used account
 	</li>
 <br>
 	<li>
-	<code> set <name> startCleaning</code>
+	<code> set &lt;name&gt; startCleaning</code>
 	<br>
 	start the Cleaning from the scratch. Depending on Model, there are additional Arguments available: eco/turbo ; normal/extraCare
 	</li>
 <br>
 	<li>
-	<code> set <name> startSpot</code>
+	<code> set &lt;name&gt; startSpot</code>
 	<br>
 	start spot-Cleaning from actual position. Depending on Model, there are additional Arguments available: eco/turbo ; normal/extraCare
 	</li>
 <br>
 	<li>
-	<code> set <name> statusRequest</code>
+	<code> set &lt;name&gt; statusRequest</code>
 	<br>
 	pull update of all readings. necessary because NEATO/VORWERK does not send updates at their own.
 	</li>
 <br>
 	<li>
-	<code> set <name> stop cleaning</code>
+	<code> set &lt;name&gt; stop cleaning</code>
 	<br>
 	stop cleaning
 	</li>
 <br>
 	<li>
-	<code> set <name> syncRobots</code>
+	<code> set &lt;name&gt; syncRobots</code>
 	<br>
 	sync robot data with online account. Useful if one has more then one robot registered
 	</li>
 <br>
 	<li>
-	<code> set <name> stopCleaning</code>
+	<code> set &lt;name&gt; stopCleaning</code>
 	<br>
 	stopCleaning and stay where you are
 	</li>
@@ -197,9 +211,16 @@
 	</li>
 <br>
 	<li>
-	boundaries
+	<code>boundaries</code>
 	<br>
-	boundaries
+  Boundary entries separated by space in JSON format, e.g.<br>
+  {"type":"polyline","vertices":[[0.710,0.6217],[0.710,0.6923]],"name":"Bad","color":"#E54B1C","enabled":true}<br>
+  {"type":"polyline","vertices":[[0.7139,0.4101],[0.7135,0.4282],[0.4326,0.3322],[0.4326,0.2533],[0.3931,0.2533],
+    [0.3931,0.3426],[0.7452,0.4637],[0.7617,0.4196]],"name":"Kueche","color":"#000000","enabled":true}<br>
+  For description of syntax see: <a href>https://developers.neatorobotics.com/api/robot-remote-protocol/maps</a><br>
+  The value of paramter "name" is used as setListe for "setBoundariesOnFloorplan_&lt;floor plan&gt;".
+  It is also possible to save more than one boundary with the same name.
+  The command "setBoundariesOnFloorplan_&lt;floor plan&gt; &lt;name&gt;" sends all boundary with the same name.
 	</li>
 <br>
 </ul>
